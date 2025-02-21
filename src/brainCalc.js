79e86395d04d99ed007b143ad9e37d2aeb9cbc8a
Natalia Romashev <tatasay@mail.ru>
2025-02-21 17:04:24 +0300

    Adding new lines of code

--- src/brainCalc.js
+++ src/brainCalc.js
@@ -1,22 +1,29 @@
 import readlineSync from 'readline-sync';
 
+const getRandomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
+
+ const getOperation = () => {
+     const operations = ['+', '-', '*'];
+     return operations[0, operations.length - 1];
+ }
+
 const brainCalc = () => {
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name?');
     console.log(`Hello, ${name}!`);
     console.log('What is the result of the expression?');
 
-    const getRandomNum = (min, max) => {
-       return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    const getOperation = () => {
-        const operations = ['+', '-', '*'];
-        return operations[0, operations.length - 1];
-    }
+    let correctAnswers = 0;
    
-    for (let i = 0; i < 3; i++) {
- 
+    for (let i = 0; correctAnswers < 3; i++) {
+        
+        const num1 = getRandomNum(1, 10);
+        const num2 = getRandomNum(1, 10);
+        const operation = getOperation();
+        const correctAnswer = `${num1}${operation}${num2}`;
+        const userAnswer =  readlineSync.question(`Question: ${num1}${operation}${num2}\nYour answer:`);
 
         if (userAnswer === correctAnswer) {
             console.log('Correct!');
