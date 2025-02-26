e7f647a8d0b1a81a102521f25a6fe65c146d954f
Natalia Romashev <tatasay@mail.ru>
2025-02-26 23:08:40 +0300

    New file

---
+++ src/games/barin-even.js
@@ -0,0 +1,20 @@
+// import readlineSync from 'readline-sync';
+import { getRandomNum, playTemplateGame } from '../index.js';
+
+const isEven = (number) => {
+    return number % 2 === 0;
+}
+
+const getGeneration = () => {
+    const number = getRandomNum(1, 50);
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    const question = `Question: ${number}`;
+    return [correctAnswer, question];
+}
+
+const playGame = () => {
+    const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+    playTemplateGame(rules, getGeneration);
+
+}
+export default playGame;
