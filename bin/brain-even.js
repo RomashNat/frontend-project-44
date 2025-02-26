ff3cfa365fe2462bcb2eb021d07a311c43341d1c
Natalia Romashev <tatasay@mail.ru>
2025-02-26 22:39:35 +0300

    Made modifications in the files for simplify the logic of the code

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import playGame from '../src/games/isEven.js';
+import playGame from '../src/games/brain-even.js';
 
 playGame();
--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
+import readlineSync from 'readline-sync';
+
 const getRandomNum = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const getOperation = () => {
-    const operations = ['+', '-', '*'];
-    return operations[getRandomNum(0, operations.length - 1)];
-}
+const playTemplateGame = (rules, getGeneration, isCheck) => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log(rules);
 
-const calculateResult = (num1, num2, operation) => {
-    switch (operation) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
+    for (let correctAnswers = 0; correctAnswers < 3; correctAnswers++) {
+        const [correctAnswer, question] = getGeneration();
+        console.log(question);
+        const userAnswer = readlineSync.question('Your answer: ');
+
+        if (parseInt(userAnswer) !== correctAnswer) {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+        console.log("Correct!");
     }
+    console.log(`Congratulations, ${name}!`);
 }
 
-export { getRandomNum, getOperation, calculateResult };
+export { getRandomNum, playTemplateGame };
